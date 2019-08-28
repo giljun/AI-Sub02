4bf9b3e97cec4b74d01ad24b116dd03dbd9dc17a
권오현 <mmdkansk@gmail.com>
2019-08-28 15:21:03 +0900

    [OH] | 2019-08-28 | Req.1-1 ~ Req.1-5

--- AI_sub2/movie_rating.py
+++ AI_sub2/movie_rating.py
@@ -10,109 +10,90 @@ from sklearn.linear_model import LogisticRegression
 Req 1-1-1. 데이터 읽기
 read_data(): 데이터를 읽어서 저장하는 함수
 """
-
 def read_data(filename):
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, encoding='utf-8', mode='r') as f:
         data = [line.split('\t') for line in f.read().splitlines()]
-        # txt 파일의 헤더(id document label)는 제외하기
-        data = data[1:]
+        data = np.array(data[1:])
     return data
-
 
 """
 Req 1-1-2. 토큰화 함수
 tokenize(): 텍스트 데이터를 받아 KoNLPy의 okt 형태소 분석기로 토크나이징
 """
-
 def tokenize(doc):
     okt = Okt()
-    return ['/'.join(t) for t in okt.pos(doc, norm=True, stem=True)]
+    return okt.morphs(doc)
 
-"""
-데이터 전 처리
-"""
-
-# train, test 데이터 읽기
+# 데이터 전 처리 train, test 데이터 읽기
 train_data = read_data('test.txt')
 test_data = read_data('test.txt')
-#  train_data = read_data('ratings_train.txt')
-# test_data = read_data('ratings_test.txt')
 
-
-# Req 1-1-2. 문장 데이터 토큰화
-# train_docs, test_docs : 토큰화된 트레이닝, 테스트  문장에 label 정보를 추가한 list
-train_docs = [(tokenize(row[1])) for row in train_data]
-test_docs = [(tokenize(row[1])) for row in test_data]
+"""
+Req 1-1-2. 문장 데이터 토큰화
+train_docs, test_docs : 토큰화된 트레이닝, 테스트  문장에 label 정보를 추가한 list
+"""
+train_docs = [tokenize(row[1]) for row in train_data]
+test_docs = [tokenize(row[1]) for row in test_data]
 
 # Req 1-1-3. word_indices 초기화
 word_indices = {}
 
 # Req 1-1-3. word_indices 채우기
-for token in train_docs:
-    for str in token:
-        tmp = str.split("/")
-        if tmp[0] not in word_indices:
-            word_indices[tmp[0]] = len(word_indices)
+for list in train_docs:
+    for token in list:
+        if token not in word_indices.keys():
+            word_indices[token]=len(word_indices)
 
 print(word_indices)
 
-# Req 1-1-4. sparse matrix 초기화
-# X: train feature data
-# X_test: test feature data
+"""
+Req 1-1-4. sparse matrix 초기화
+X: train feature data
+X_test: test feature data
+"""
 X = lil_matrix((len(train_docs), len(word_indices)))
 X_test = lil_matrix((len(test_docs), len(word_indices)))
 
 # 평점 label 데이터가 저장될 Y 행렬 초기화
 # Y: train data label
 # Y_test: test data label
-Y = lil_matrix((len(train_data), 1))
-Y_test = lil_matrix((len(test_data), 1))
+Y = np.array(train_data[: , 2])
+Y_test = np.array(test_data[: , 2])
 
 # Req 1-1-5. one-hot 임베딩
 # X,Y 벡터값 채우기
-index = 0
+for idx, lists in enumerate(train_docs):
+    for token in lists:
+        X[idx, word_indices[token]] = 1
 
-for token in train_docs:
-    for str in token:
-        tmp = str.split("/")
-        X[index, word_indices[tmp[0]]] = 1
-    index = index+1
 
-index = 0
-for i in train_data:
-    Y[index,0] = i[2]
-index = 0
-for i in test_data:
-    Y_test[index,0] = i[2]
-
-print(X.todense())
-print(Y.todense())
 
 """
 트레이닝 파트
 clf  <- Naive baysian mdoel
 clf2 <- Logistic regresion model
 """
-print(Y.todense())
+
 # Req 1-2-1. Naive baysian mdoel 학습
 clf = MultinomialNB()
-clf.fit(X.todense(), Y.todense())
+clf.fit(X.todense(), Y)
 
 # Req 1-2-2. Logistic regresion mdoel 학습
 clf2 = LogisticRegression()
-clf2.fit(X.todense(), Y.todense())
+clf2.fit(X.todense(), Y)
 
 
 """
 테스트 파트
 """
-
 # Req 1-3-1. 문장 데이터에 따른 예측된 분류값 출력
 print("Naive bayesian classifier example result: {}, {}".format(test_data[1][1], clf.predict(X_test[1])))
 print("Logistic regression exampleresult: {}, {}".format(test_data[1][1],clf2.predict(X_test[1])))
 # Req 1-3-2. 정확도 출력
 print("Naive bayesian classifier accuracy: {}".format(None))
 print("Logistic regression accuracy: {}".format(None))
+
+
 
 """
 데이터 저장 파트
