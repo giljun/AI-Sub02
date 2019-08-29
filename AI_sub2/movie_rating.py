b6c51dcf7974618ca93958ff31fe2a4546aceb20
박길준 <adda281@naver.com>
2019-08-29 09:18:30 +0900

    20190829|S1P2242008-2|Req 1-1-3 word_indices 채우기 완료

--- AI_sub2/movie_rating.py
+++ AI_sub2/movie_rating.py
@@ -20,7 +20,7 @@ read_data(): 데이터를 읽어서 저장하는 함수
 """
 
 def read_data(filename):
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         data = [line.split('\t') for line in f.read().splitlines()]
         # txt 파일의 헤더( id document label )는 제외하기
         data = data[1:]
@@ -42,21 +42,21 @@ def tokenize(doc):
 """
 
 # train, test 데이터 읽기
-# train_data = read_data('ratings_train.txt')
-# test_data = read_data('ratings_test.txt')
+train_data = read_data('ratings_train.txt')
+test_data = read_data('ratings_test.txt')
 
 # 나중에 위의 코드를 사용할 예정
-train_data = read_data('test.txt')
-test_data = read_data('test.txt')
+# train_data = read_data('test.txt')
+# test_data = read_data('test.txt')
 
 # Req 1-1-2. 문장 데이터 토큰화
 # train_docs, test_docs : 토큰화된 트레이닝, 테스트  문장에 label 정보를 추가한 list
 # train_docs = None
 # test_docs = None
 if os.path.isfile('train_docs.json'):
-    with open('train_docs.json') as f:
+    with open('train_docs.json', 'r', encoding="utf-8") as f:
         train_docs = json.load(f)
-    with open('test_docs.json') as f:
+    with open('test_docs.json', 'r', encoding="utf-8") as f:
         test_docs = json.load(f)
 else:
     train_docs = [(tokenize(row[1]), row[2]) for row in train_data]
@@ -67,20 +67,23 @@ else:
     with open('test_docs.json', 'w', encoding="utf-8") as make_file:
         json.dump(test_docs, make_file, ensure_ascii=False, indent="\t")
 
-pprint(train_docs[0])
-
-
 # Req 1-1-3. word_indices 초기화
 word_indices = {}
 
 # Req 1-1-3. word_indices 채우기
+for tokens, index in train_docs:
+    for token in tokens:
+        temp = token.split('/')
+        if temp[0] not in word_indices:
+            word_indices[temp[0]] = len(word_indices)
+
+# 임시 테스트
 
 # Req 1-1-4. sparse matrix 초기화
 # X: train feature data
 # X_test: test feature data
 X = None
 X_test = None
-
 
 # 평점 label 데이터가 저장될 Y 행렬 초기화
 # Y: train data label
